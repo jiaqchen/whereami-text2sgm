cde835e5e8b1aa34f48dd8fc7bf3cdcbeedd3cf8
Jiaqi Chen <jiaqchen@ethz.ch>
2024-01-06 16:29:44 +0100

    dbscan not so strict so subgraphs have more nodes

--- data_distribution_analysis/helper.py
+++ data_distribution_analysis/helper.py
@@ -7,9 +7,7 @@ import copy
 
 sys.path.insert(0, '/home/julia/Documents/h_coarse_loc/playground')
 sys.path.insert(1, '/home/julia/Documents/h_coarse_loc/playground/graph_models/data_processing')
-import graph_models.data_processing.sg_dataloader as sg_dataloader
-from sg_dataloader import SceneGraph
-from graph_models.src.utils import word_similarity
+from scene_graph import SceneGraph
 
 import spacy
 import en_core_web_lg
@@ -39,64 +37,73 @@ def combine_node_features(graph1, graph2):
 def get_matching_subgraph(graph1, graph2):
     # Cluster the nodes in both graphs with dbscan
     all_node_features, all_node_graph_index = combine_node_features(graph1, graph2)
+    combined_node_idx = np.concatenate(([n1 for n1 in graph1.nodes], [n2 for n2 in graph2.nodes]), axis=0)
+    assert(all([i == graph1.nodes[i].idx for i in graph1.nodes])) # key equals the idx
+    assert(all([i == graph2.nodes[i].idx for i in graph2.nodes]))
+    idx_mapping = {}
+    for i, idx in enumerate(combined_node_idx):
+        idx_mapping[i] = idx
 
     # Track the indices of the nodes that are matched, after combining into all_node_features
-    clustering = DBSCAN(eps=0.05, min_samples=1, metric='cosine').fit(all_node_features)
+    clustering = DBSCAN(eps=0.25, min_samples=1, metric='cosine').fit(all_node_features) # default 0.05
     clusters = {}
-    for idx, cluster in enumerate(clustering.labels_):
+    for i, cluster in enumerate(clustering.labels_):
         if cluster in clusters:
-            clusters[cluster].append(idx)
+            clusters[cluster].append(i)
         else:
-            clusters[cluster] = [idx]
+            clusters[cluster] = [i]
 
     # Process the clusters so that only clusters with nodes from both graphs remain
     graph1_keep_nodes = []
     graph2_keep_nodes = []
     for cluster in clusters:
         indices = clusters[cluster]
-        graphs = [all_node_graph_index[idx] for idx in indices]
+        graphs = [int(all_node_graph_index[i]) for i in indices]
         if 0 in graphs and 1 in graphs:
-            graph1_keep_nodes.extend([idx for idx in indices if all_node_graph_index[idx] == 0])
-            graph2_keep_nodes.extend([idx for idx in indices if all_node_graph_index[idx] == 1])
-            continue
-    assert(graph1_keep_nodes is not None)
-    assert(graph2_keep_nodes is not None)
+            graph1_keep_nodes.extend([idx_mapping[i] for i in indices if int(all_node_graph_index[i]) == 0])
+            graph2_keep_nodes.extend([idx_mapping[i] for i in indices if int(all_node_graph_index[i]) == 1])
+
     # Get the subgraph
     assert(type(graph1) == SceneGraph)
     assert(type(graph2) == SceneGraph)        
     graph1_keep_nodes = list(set(graph1_keep_nodes))
     graph2_keep_nodes = list(set(graph2_keep_nodes))
-    subgraph1 = graph1.get_subgraph(graph1_keep_nodes)
-    subgraph2 = graph2.get_subgraph(graph2_keep_nodes)
+    subgraph1 = graph1.get_subgraph(graph1_keep_nodes, return_graph=True)
+    subgraph2 = graph2.get_subgraph(graph2_keep_nodes, return_graph=True)
 
-    # # Print graph nodes and subgraph nodes
-    # print("Graph1 nodes: ")
-    # for node in graph1.nodes:
-    #     print(node.label)
-    # print("Graph2 nodes: ")
-    # for node in graph2.nodes:
-    #     print(node.label)
-    # print("Subgraph1 nodes: ")
-    # for node in subgraph1.nodes:
-    #     print(node.label)
-    # print("Subgraph2 nodes: ")
-    # for node in subgraph2.nodes:
-    #     print(node.label)
-
-
-    assert(subgraph1 is not None)
-    assert(subgraph2 is not None)
+    # if subgraph1 is not None and subgraph2 is not None:
+    #     # Print graph nodes and subgraph nodes
+    #     print("Graph1 nodes: ")
+    #     for nodeid in graph1.nodes:
+    #         node = graph1.nodes[nodeid]
+    #         print(node.label)
+    #     print("Graph2 nodes: ")
+    #     for nodeid in graph2.nodes:
+    #         node = graph2.nodes[nodeid]
+    #         print(node.label)
+    #     print("Subgraph1 nodes: ")
+    #     for nodeid in subgraph1.nodes:
+    #         node = subgraph1.nodes[nodeid]
+    #         print(node.label)
+    #     print("Subgraph2 nodes: ")
+    #     for nodeid in subgraph2.nodes:
+    #         node = subgraph2.nodes[nodeid]
+    #         print(node.label)
 
     return subgraph1, subgraph2
 
-def calculate_overlap(graph1, graph2):
+def calculate_overlap(graph1, graph2, sim_thr=0.95):
     # Go through the nodes of the two scene graphs, where overlap is calculated as out of graph1's nodes
     # how many of them are also in graph2
+    if graph1 is None or graph2 is None:
+        return 0
     overlap = 0
-    for node1 in graph1.nodes:
+    for node1id in graph1.nodes:
+        node1 = graph1.nodes[node1id]
         found_match = False
-        for node2 in graph2.nodes:
-            if nodes_features_similar(node1, node2, sim_thr=0.85):
+        for node2id in graph2.nodes:
+            node2 = graph2.nodes[node2id]
+            if nodes_features_similar(node1, node2, sim_thr):
                 found_match = True
                 break
         if found_match:
