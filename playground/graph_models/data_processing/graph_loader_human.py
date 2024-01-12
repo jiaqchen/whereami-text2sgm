1d419939cf5fadf96dd011c66a52c44a3000dba3
Jiaqi Chen <jiaqchen@ethz.ch>
2024-01-12 10:44:54 +0100

    graph_loader_human for getting the human.json into a graph readable format

---
+++ playground/graph_models/data_processing/graph_loader_human.py
@@ -0,0 +1,66 @@
+import os
+import json
+import torch
+from tqdm import tqdm
+import re
+
+from graph_loader_utils import get_ada, get_word2vec, check_and_remove_invalid_edges
+from graph_models.src.utils import txt_to_json
+
+def process_scenes_to_dict(dir_to_scenes):
+    ids = os.listdir(dir_to_scenes)
+    scenes = {}
+    for id in tqdm(ids):
+        # print(f'Processing scene {id}')
+        try:
+            scene_dir = sorted([int(x[:-5]) for x in os.listdir(os.path.join(dir_to_scenes, id))])
+            scene = {}
+            for file in scene_dir:
+                filename = str(file) + '.json'
+                with open(os.path.join(dir_to_scenes, id, filename)) as f:
+                    data = f.read()
+                    data = txt_to_json(data)
+                    data = json.loads(data)
+                    scene[file] = data
+        except Exception as e:
+            print(f'Error processing scene {id}: {e}')
+
+        scenes[id] = scene
+    return scenes
+                
+def add_edge_features(all_scenes):
+    hada = {}
+    hw2v = {}
+    for scene_id in tqdm(all_scenes):
+        for edge in all_scenes[scene_id]['edges']:
+            edge['relation_ada'], hada = get_ada(edge['relationship'], hada)
+            edge['relation_word2vec'], hw2v = get_word2vec(edge['relationship'], hw2v)
+            # NOTE: assumed there are no attributes for edges
+    return all_scenes
+
+def add_node_features(all_scenes):
+    hada = {}
+    hw2v = {}
+    for scene_id in tqdm(all_scenes):
+        for node in all_scenes[scene_id]['nodes']:
+            node['label_ada'], hada = get_ada(node['label'], hada)
+            node['label_word2vec'], hw2v = get_word2vec(node['label'], hw2v)
+            attributes_ada = {'all': []}
+            attributes_word2vec = {'all': []}
+            for attribute in node['attributes']:
+                attr_ada, hada = get_ada(attribute, hada)
+                attr_word2vec, hw2v = get_word2vec(attribute, hw2v)
+                attributes_ada['all'].append(attr_ada)
+                attributes_word2vec['all'].append(attr_word2vec)
+            node['attributes_ada'] = attributes_ada
+            node['attributes_word2vec'] = attributes_word2vec
+    return all_scenes
+
+if __name__ == '__main__':
+    # all_scenes = torch.load('/home/julia/Documents/h_coarse_loc/playground/graph_models/data_checkpoints/processed_data/human/human_graphs_unprocessed.pt')
+    # all_scenes = add_node_features(all_scenes)
+    # all_scenes = add_edge_features(all_scenes)
+    # torch.save(all_scenes, '/home/julia/Documents/h_coarse_loc/playground/graph_models/data_checkpoints/processed_data/human/human_graphs_processed.pt')
+
+    all_scenes = torch.load('/home/julia/Documents/h_coarse_loc/playground/graph_models/data_checkpoints/processed_data/human/human_graphs_processed.pt')
+    print(f'keys: {all_scenes.keys()}')
