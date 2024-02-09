45146622a9f1eecac465ae069c5ff7e7bb860199
Jiaqi Chen <jiaqchen@eu-login-07.euler.ethz.ch>
2024-02-09 19:31:07 +0100

    add shell scripts for running on euler

---
+++ shell/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# SCRIPTPATH=$(dirname $(readlink -f "$0"))
+PROJECT_DIR="/cluster/project/cvg/jiaqchen/h_coarse_loc/playground/graph_models/models/"
+
+# export PYTHONPATH=$PROJECT_DIR:$PYTHONPATH
+cd $PROJECT_DIR
+
+python3 train.py \
+    --epoch 100 \
+    --N 1 \
+    --lr 0.0001 \
+    --weight_decay 0.00005 \
+    --batch_size 16 \
+    --graph_size_min 5 \
+    --contrastive_loss True \
+    --valid_top_k 1 2 3 5 \
+    --use_attributes True \
+    --training_with_cross_val True \
+    --folds 10 \
+    --skip_k_fold True
